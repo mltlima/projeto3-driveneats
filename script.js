9da66b6f88ff018a422e41064569e7d2a1ec89ab
mltlima <miguellt@dcc.ufrj.br>
2022-01-30 00:49:04 -0300

    opcao de mandar mensagem por whatsapp adicionado

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ const itemsPedidos = {
 
     valorTotal: 0,
 
-    nome: '',
-    endereco: '',
+    nome: null,
+    endereco: null,
 }
 
 
@@ -23,6 +23,8 @@ function selecionarPratoPrincipal(pratoPedido) {
     pratoPedido.querySelector("ion-icon").classList.remove("escondido");
     pratoPedido.classList.add("item-selecionado")
     itemsPedidos.pratoPrincipal = pratoPedido;
+    const preco = parseFloat(pratoPedido.querySelector("h3").innerHTML.split(' ')[1]);
+    itemsPedidos.pratoPrincipalPreco = preco;
     verificaPedido()
 }
 
@@ -36,6 +38,8 @@ function selecionarBebida(bebida){
     bebida.querySelector("ion-icon").classList.remove("escondido");
     bebida.classList.add("item-selecionado")
     itemsPedidos.bebida = bebida;
+    const preco = parseFloat(bebida.querySelector("h3").innerHTML.split(' ')[1]);
+    itemsPedidos.bebidaPreco = preco;
     verificaPedido()
 }
 
@@ -49,6 +53,8 @@ function selecionarSobremesa(sobremesa) {
     sobremesa.querySelector("ion-icon").classList.remove("escondido");
     sobremesa.classList.add("item-selecionado")
     itemsPedidos.sobremesa = sobremesa;
+    const preco = parseFloat(sobremesa.querySelector("h3").innerHTML.split(' ')[1]);
+    itemsPedidos.sobremesaPreco = preco;
     verificaPedido()
 }
 
@@ -56,9 +62,33 @@ function selecionarSobremesa(sobremesa) {
 function verificaPedido() {
 
     const confirmar = document.querySelector("footer button")
-    
     if (itemsPedidos.pratoPrincipal != null && itemsPedidos.bebida != null && itemsPedidos.sobremesa != null) {
+        //alert(itemsPedidos.valorTotal.toFixed(2));
+        itemsPedidos.valorTotal = itemsPedidos.bebidaPreco + itemsPedidos.pratoPrincipalPreco + itemsPedidos.sobremesaPreco;
         confirmar.classList.add("botao-confirmar");
         confirmar.innerHTML = "Fechar Pedido";
+        confirmar.disabled = false;
     }
+}
+
+function fecharPedido() {
+    
+    let uri =   `Olá, gostaria de fazer o pedido:
+                - Prato: ${itemsPedidos.pratoPrincipal.querySelector("h1").innerHTML}
+                - Bebida: ${itemsPedidos.bebida.querySelector("h1").innerHTML}
+                - Sobremesa: ${itemsPedidos.sobremesa.querySelector("h1").innerHTML}
+                Total: R$${itemsPedidos.valorTotal}`
+    
+    let encoded = encodeURIComponent(uri);
+    window.open(`https://wa.me/553131313131?text=${encoded}`);
+    //alert(uri);
+}
+
+function adicionarDados() {
+
+    itemsPedidos.pratoPrincipalPreco
+}
+
+function confirmaPedido() {
+
 }
